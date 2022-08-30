94fe23fbffd214e21defa6a5383086a8d45d825b
Filip Brkić <filip.brkic@barrage.net>
2022-08-30 14:10:39 +0200

    implemented cypress

---
+++ cypress/e2e/brands.cy.js
@@ -0,0 +1,17 @@
+describe('Brands', () => {
+  beforeEach(() => {
+    cy.request("http://127.0.0.1:8080/brands");
+    cy.request("POST", "http://127.0.0.1:8080/brands", {
+      name: "cypress",
+      description: "cypress"
+    });
+    cy.request("PUT", "http://127.0.0.1:8080/brands/101", {
+      name: "cypress",
+      description: "cypress"
+    });
+   // cy.request("DELETE", "http://127.0.0.1:8080/brands/102"); // works only once
+  })
+  it('successfully loads', () => {
+    cy.visit('/brands');
+  })
+})
